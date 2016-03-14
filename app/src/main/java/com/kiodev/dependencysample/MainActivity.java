9cfa76f4154be1ea9638518f3a9d2e6a7535edad
Kio Krofovitch <kio.krofovitch@gmail.com>
2016-03-13 21:47:17 -0600

    Include library using jitpack

--- app/src/main/java/com/kiodev/dependencysample/MainActivity.java
+++ app/src/main/java/com/kiodev/dependencysample/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.kiodev.mylibrary.Pokemon;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Test library
+        ArrayList<Pokemon> pokemonArrayList = new ArrayList<>();
+
+        Pokemon pikachu = new Pokemon("Pikachu", "Electric");
+        pikachu.setNumber(25);
+
+        pokemonArrayList.add(pikachu);
     }
 
     @Override
